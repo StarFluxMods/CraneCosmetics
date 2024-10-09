199787e64af522a56a6b34f770e0c89db9f418f1
Lachlan Leone <starfluxgames@gmail.com>
2024-10-09 16:40:30 +1100

    v0.1.1

--- Mod.cs
+++ Mod.cs
@@ -13,7 +13,7 @@ namespace CraneCosmetics
     {
         public const string MOD_GUID = "com.starfluxgames.cranecosmetics";
         public const string MOD_NAME = "Crane Cosmetics";
-        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_VERSION = "0.1.1";
         public const string MOD_AUTHOR = "StarFluxGames";
         public const string MOD_GAMEVERSION = ">=1.2.0";
 
--- Systems/CreateCraneCosmeticStation.cs
+++ Systems/CreateCraneCosmeticStation.cs
@@ -1,4 +1,5 @@
-﻿using CraneCosmetics.Customs.Appliances;
+﻿using System;
+using CraneCosmetics.Customs.Appliances;
 using Kitchen;
 using KitchenLib.Utils;
 using KitchenMods;
@@ -8,14 +9,15 @@ using UnityEngine;
 
 namespace CraneCosmetics.Systems
 {
+    [UpdateAfter(typeof(CreatePracticeTrigger))]
+    [UpdateAfter(typeof(CreateRerollTrigger))]
     public class CreateCraneCosmeticStation : NightSystem, IModSystem
     {
-        private EntityQuery _practiceModeTrigger;
-        
+        private EntityQuery _cPositions;
         protected override void Initialise()
         {
             base.Initialise();
-            _practiceModeTrigger = GetEntityQuery(typeof(CAppliance), typeof(CTriggerPracticeMode));
+            _cPositions = GetEntityQuery(typeof(CPosition));
         }
 
         protected override void OnUpdate()
@@ -24,23 +26,82 @@ namespace CraneCosmetics.Systems
             {
                 return;
             }
-            
-            using (NativeArray<Entity> practiceModeTriggers = _practiceModeTrigger.ToEntityArray(Allocator.Temp))
+
+            if (GetNextAvailableCPosition(4, out Vector3 result, 10))
             {
-                foreach (Entity practiceModeTrigger in practiceModeTriggers)
+                Entity entity = EntityManager.CreateEntity(typeof(CCreateAppliance), typeof(CPosition), typeof(SCreateCraneCosmeticStation), typeof(CDoNotPersist));
+                EntityManager.SetComponentData(entity, new CCreateAppliance
                 {
-                    if (Require(practiceModeTrigger, out CPosition position))
+                    ID = GDOUtils.GetCustomGameDataObject<CraneCosmeticStation>().ID
+                });
+                EntityManager.SetComponentData(entity, new CPosition(result));
+            }
+            else
+            {
+                Mod.Logger.LogError("Failed to find viable spawn position");
+            }
+        }
+
+        public bool GetNextAvailablePosition(int depth, out Vector3 result, int maxdepth = 10)
+        {
+            if (depth > maxdepth)
+            {
+                result = Vector3.zero;
+                return false;
+            }
+
+            Vector3 frontDoor = GetFrontDoor();
+            int num = frontDoor.x > 0f ? -1 : 1;
+
+            Vector3 checkPosition = frontDoor + new Vector3(num * depth, 0f, -1f);
+            
+            if (TileManager.GetOccupant(checkPosition) != Entity.Null)
+            {
+                return GetNextAvailablePosition(depth + 1, out result, maxdepth);
+            }
+
+            result = checkPosition;
+            return true;
+        }
+        
+        public bool GetNextAvailableCPosition(int depth, out Vector3 result, int maxdepth = 10)
+        {
+            if (depth > maxdepth)
+            {
+                result = Vector3.zero;
+                return false;
+            }
+
+            Vector3 frontDoor = GetFrontDoor();
+            int num = frontDoor.x > 0f ? -1 : 1;
+
+            Vector3 checkPosition = frontDoor + new Vector3(num * depth, 0f, -1f);
+            
+            if (IsPositionInUse(checkPosition))
+            {
+                return GetNextAvailableCPosition(depth + 1, out result, maxdepth);
+            }
+
+            result = checkPosition;
+            return true;
+        }
+
+        public bool IsPositionInUse(Vector3 checkPosition, float TOLERANCE = 0.1f)
+        {
+            using (NativeArray<Entity> cPositions = _cPositions.ToEntityArray(Allocator.Temp))
+            {
+                foreach (Entity cPosition in cPositions)
+                {
+                    if (Require(cPosition, out CPosition position))
                     {
-                        Entity entity = EntityManager.CreateEntity(typeof(CCreateAppliance), typeof(CPosition), typeof(SCreateCraneCosmeticStation), typeof(CDoNotPersist));
-                        EntityManager.SetComponentData(entity, new CCreateAppliance
+                        if ((Math.Abs(checkPosition.x - position.Position.x) < TOLERANCE) && (Math.Abs(checkPosition.y - position.Position.y) < TOLERANCE) && (Math.Abs(checkPosition.z - position.Position.z) < TOLERANCE))
                         {
-                            ID = GDOUtils.GetCustomGameDataObject<CraneCosmeticStation>().ID
-                        });
-                        int num = ((position.Position.x > 0f) ? (-1) : 1);
-                        EntityManager.SetComponentData(entity, new CPosition(position.Position + new Vector3(num * 1, 0f, 0)));
+                            return true;
+                        }
                     }
                 }
             }
+            return false;
         }
 
         private struct SCreateCraneCosmeticStation : IComponentData, IModComponent
