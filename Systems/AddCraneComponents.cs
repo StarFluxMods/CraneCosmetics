95e02f452e528adbe72b26b42a2cd7a84750a816
Lachlan Leone <starfluxgames@gmail.com>
2024-10-12 16:15:44 +1100

    v0.1.2

--- Systems/AddCraneComponents.cs
+++ Systems/AddCraneComponents.cs
@@ -1,4 +1,5 @@
-﻿using CraneCosmetics.Components;
+﻿using System.Collections.Generic;
+using CraneCosmetics.Components;
 using Kitchen;
 using KitchenMods;
 using Unity.Collections;
@@ -25,6 +26,43 @@ namespace CraneCosmetics.Systems
                         EntityManager.AddComponentData(player, new CCraneCosmetics());
                 }
             }
+        }
+
+        private Dictionary<int, CCraneCosmetics> CCraneCosmeticsCache = new Dictionary<int, CCraneCosmetics>();
+
+        public override void BeforeSaving(SaveSystemType system_type)
+        {
+            base.AfterSaving(system_type);
+            CCraneCosmeticsCache.Clear();
+            using (NativeArray<Entity> players = _players.ToEntityArray(Allocator.Temp))
+            {
+                foreach (Entity player in players)
+                {
+                    if (!Require(player, out CCraneCosmetics cCraneCosmetics) || !Require(player, out CPlayer cPlayer)) continue;
+                    if (CCraneCosmeticsCache.ContainsKey(cPlayer.ID)) continue;
+                    CCraneCosmeticsCache.Add(cPlayer.ID, cCraneCosmetics);
+                    EntityManager.RemoveComponent<CCraneCosmetics>(player);
+                }
+            }
+        }
+
+        public override void AfterSaving(SaveSystemType system_type)
+        {
+            base.AfterSaving(system_type);
+            using (NativeArray<Entity> players = _players.ToEntityArray(Allocator.Temp))
+            {
+                foreach (Entity player in players)
+                {
+                    if (Require(player, out CPlayer cPlayer) && !Require(player, out CCraneCosmetics cCraneCosmetics))
+                    {
+                        if (CCraneCosmeticsCache.ContainsKey(cPlayer.ID))
+                        {
+                            EntityManager.AddComponentData(player, CCraneCosmeticsCache[cPlayer.ID]);
+                        }
+                    }
+                }
+            }
+            CCraneCosmeticsCache.Clear();
         }
     }
 }
